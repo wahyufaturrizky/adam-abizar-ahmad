92c74001ad96c1bf0793fcaa12cf5a3f3e0e5326
wahyu fatur rizki <wahyu.rizky@docdoc.com>
2022-07-07 01:45:16 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-mobile">
+        <div className="hero-banner">
+          <img
+            src="https://i.ibb.co/7QQwLRR/photo-2022-07-05-20-34-51.jpg"
+            alt=""
+            width={368}
+            height={730}
+          />
+          <div className="child-hero-banner">
+            <h3 className="title">Adam Abizar Ahmad</h3>
+          </div>
+        </div>
+        <div className="short-intro">Short Intro</div>
+        <div className="schedule">schedule</div>
+        <div className="location">Location</div>
+        <div className="gallery">gallery</div>
+        <div className="whises">whises</div>
+      </div>
     </div>
   );
 }
